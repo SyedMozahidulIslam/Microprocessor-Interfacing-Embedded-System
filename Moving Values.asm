b3f9b42a76e30a4947351d9a489d54f263de2e96
Syed Mozahidul Islam Fahim <116684492+SyedMozahidulIslam@users.noreply.github.com>
2024-02-20 20:56:02 +0600

    Add files via upload

---
+++ Moving Values.asm
@@ -0,0 +1,15 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+mov ax,2 
+mov bx,2 
+add ax, bx
+mov cx, ax
+ret
+
+
+
+
